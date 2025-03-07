b817e63ece41a1b406bc57045f47e42ea215313c
venopyX <gladsonchala@gmail.com>
2025-03-07 17:54:03 +0300

    feat: modify app routes to include qubee quest

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -2,10 +2,16 @@
 import 'package:flutter/material.dart';
 import '../../features/hibboo/presentation/pages/hibboo_dashboard_page.dart';
 import '../../features/hibboo/presentation/pages/hibboo_playing_page.dart';
+import '../../features/qubee_quest/presentation/pages/qubee_quest_map_page.dart';
+import '../../features/qubee_quest/presentation/pages/qubee_quest_land_page.dart';
+import '../../features/qubee_quest/presentation/pages/qubee_quest_letter_page.dart';
 
 class AppRoutes {
   static const String hibbooDashboard = '/hibboo_dashboard';
   static const String hibbooPlaying = '/hibboo_playing';
+  static const String qubeeQuestMap = '/qubee_quest_map';
+  static const String qubeeQuestLand = '/qubee_quest_land';
+  static const String qubeeQuestLetter = '/qubee_quest_letter';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -13,6 +19,12 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const HibbooDashboardPage());
       case hibbooPlaying:
         return MaterialPageRoute(builder: (_) => const HibbooPlayingPage());
+      case qubeeQuestMap:
+        return MaterialPageRoute(builder: (_) => const QubeeQuestMapPage());
+      case qubeeQuestLand:
+        return MaterialPageRoute(builder: (_) => const QubeeQuestLandPage());
+      case qubeeQuestLetter:
+        return MaterialPageRoute(builder: (_) => const QubeeQuestLetterPage());
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
