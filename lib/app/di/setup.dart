11af4e4ce3f53c1b619660d1cb6cc8396c6d56ef
venopyX <gladsonchala@gmail.com>
2025-03-07 17:52:12 +0300

    chore: update dependency injection setup for qubee quest

--- lib/app/di/setup.dart
+++ lib/app/di/setup.dart
@@ -4,6 +4,11 @@ import '../../features/hibboo/data/datasources/hibboo_datasource.dart';
 import '../../features/hibboo/data/repositories/hibboo_repository_impl.dart';
 import '../../features/hibboo/domain/usecases/get_hibboo_list.dart';
 import '../../features/hibboo/presentation/providers/hibboo_provider.dart';
+import '../../features/qubee_quest/data/datasources/qubee_quest_datasource.dart';
+import '../../features/qubee_quest/data/repositories/qubee_quest_repository_impl.dart';
+import '../../features/qubee_quest/domain/usecases/get_qubee_letters.dart';
+import '../../features/qubee_quest/domain/usecases/get_lands.dart';
+import '../../features/qubee_quest/presentation/providers/qubee_quest_provider.dart';
 
 // Singleton instances for Providers
 void setupDependencies() {
@@ -20,5 +25,14 @@ List<SingleChildWidget> get providers => [
       return HibbooProvider(getHibbooList);
     },
   ),
-  // Add other providers here later (e.g., AuthProvider, GameProvider)
+  // Add Qubee Quest provider
+  ChangeNotifierProvider(
+    create: (_) {
+      final datasource = QubeeQuestDatasource();
+      final repository = QubeeQuestRepositoryImpl(datasource);
+      final getQubeeLetters = GetQubeeLetters(repository);
+      final getLands = GetLands(repository);
+      return QubeeQuestProvider(getQubeeLetters, getLands);
+    },
+  ),
 ];
