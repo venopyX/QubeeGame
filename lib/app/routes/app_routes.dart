de279103c7f7d625c2158a77bf3405b579f39aad
gladsonchala <gladsonchala@gmail.com>
2025-03-11 01:26:14 +0000

    feat: Add navigation routes for Oromo Playhouse feature

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -2,10 +2,12 @@
 import 'package:flutter/material.dart';
 import '../../features/hibboo/presentation/pages/hibboo_dashboard_page.dart';
 import '../../features/hibboo/presentation/pages/hibboo_playing_page.dart';
+import '../../features/playhouse/presentation/pages/playhouse_dashboard_page.dart';
 
 class AppRoutes {
   static const String hibbooDashboard = '/hibboo_dashboard';
   static const String hibbooPlaying = '/hibboo_playing';
+  static const String playhouseDashboard = '/playhouse_dashboard';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -13,6 +15,8 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const HibbooDashboardPage());
       case hibbooPlaying:
         return MaterialPageRoute(builder: (_) => const HibbooPlayingPage());
+      case playhouseDashboard:
+        return MaterialPageRoute(builder: (_) => const PlayhouseDashboardPage());
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
