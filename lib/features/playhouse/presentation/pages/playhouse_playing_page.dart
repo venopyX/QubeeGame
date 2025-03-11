38d92a1520240f5788e6b987b40f1116496de154
gladsonchala <gladsonchala@gmail.com>
2025-03-11 12:31:12 +0000

    refactor: word guessing game removed to separate it into another feature

--- lib/features/playhouse/presentation/pages/playhouse_playing_page.dart
+++ lib/features/playhouse/presentation/pages/playhouse_playing_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/playhouse_provider.dart';
 import '../widgets/video_player_widget.dart';
-import '../widgets/word_matching_game_widget.dart';
 
 class PlayhousePlayingPage extends StatefulWidget {
   const PlayhousePlayingPage({super.key});
@@ -12,8 +11,6 @@ class PlayhousePlayingPage extends StatefulWidget {
 }
 
 class _PlayhousePlayingPageState extends State<PlayhousePlayingPage> {
-  bool _showingGame = false;
-
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<PlayhouseProvider>(context);
@@ -29,28 +26,17 @@ class _PlayhousePlayingPageState extends State<PlayhousePlayingPage> {
       );
     }
 
-    if (_showingGame) {
-      return WordMatchingGameWidget(
-        onGameComplete: (stars) {
-          provider.earnStars(stars);
-          Navigator.of(context).pop();
-        },
-        onClose: () => Navigator.of(context).pop(),
-      );
-    } else {
-      return VideoPlayerWidget(
-        video: video,
-        onComplete: () {
-          provider.completeVideo(video.id);
-          setState(() {
-            _showingGame = true;
-          });
-        },
-        onClose: () {
-          provider.clearSelectedVideo();
-          Navigator.of(context).pop();
-        },
-      );
-    }
+    return VideoPlayerWidget(
+      video: video,
+      onComplete: () {
+        provider.completeVideo(video.id);
+        provider.clearSelectedVideo();
+        Navigator.of(context).pop();
+      },
+      onClose: () {
+        provider.clearSelectedVideo();
+        Navigator.of(context).pop();
+      },
+    );
   }
 }
