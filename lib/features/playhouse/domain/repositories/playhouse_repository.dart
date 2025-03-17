c4b8d006e6defef7d7f07d7e0053b01445ef5310
venopyX <gladsonchala@gmail.com>
2025-03-17 12:51:06 +0300

    refactor: simplify PlayhouseRepository interface

--- lib/features/playhouse/domain/repositories/playhouse_repository.dart
+++ lib/features/playhouse/domain/repositories/playhouse_repository.dart
@@ -1,10 +1,7 @@
 import '../entities/video.dart';
-import '../entities/playhouse_progress.dart';
 
 abstract class PlayhouseRepository {
   Future<List<Video>> getVideos();
-  Future<void> markVideoCompleted(String videoId);
-  Future<void> addStars(int count);
-  Future<PlayhouseProgress> getProgress();
-  Future<void> saveProgress(PlayhouseProgress progress);
+  Future<String?> getLastWatchedVideoId();
+  Future<void> saveLastWatchedVideoId(String videoId);
 }
