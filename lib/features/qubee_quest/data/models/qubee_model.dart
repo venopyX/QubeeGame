4c677ea1cd831704552d51d020dfba087ca3668b
gladsonchala <gladsonchala@gmail.com>
2025-03-18 17:45:42 +0000

    feat: add QubeeModel data class

--- lib/features/qubee_quest/data/models/qubee_model.dart
+++ lib/features/qubee_quest/data/models/qubee_model.dart
@@ -2,19 +2,41 @@ import '../../domain/entities/qubee.dart';
 
 class QubeeModel extends Qubee {
   QubeeModel({
-    required super.id,
-    required super.letter,
-    required super.pronunciation,
-    required super.soundPath,
-    required super.tracingPoints,
-    required super.unlockedWords,
-    required super.requiredPoints,
-  });
+    required int id,
+    required String letter,
+    required String smallLetter,
+    required String latinEquivalent,
+    required String pronunciation,
+    required String soundPath,
+    required List<Map<String, double>> tracingPoints,
+    required List<String> unlockedWords,
+    required String exampleSentence,
+    required String meaningOfSentence,
+    required int requiredPoints,
+    bool isUnlocked = false,
+    bool isCompleted = false,
+  }) : super(
+          id: id,
+          letter: letter,
+          smallLetter: smallLetter,
+          latinEquivalent: latinEquivalent,
+          pronunciation: pronunciation,
+          soundPath: soundPath,
+          tracingPoints: tracingPoints,
+          unlockedWords: unlockedWords,
+          exampleSentence: exampleSentence,
+          meaningOfSentence: meaningOfSentence,
+          requiredPoints: requiredPoints,
+          isUnlocked: isUnlocked,
+          isCompleted: isCompleted,
+        );
 
   factory QubeeModel.fromJson(Map<String, dynamic> json) {
     return QubeeModel(
       id: json['id'],
       letter: json['letter'],
+      smallLetter: json['smallLetter'],
+      latinEquivalent: json['latinEquivalent'],
       pronunciation: json['pronunciation'],
       soundPath: json['soundPath'],
       tracingPoints: List<Map<String, double>>.from(
@@ -24,7 +46,11 @@ class QubeeModel extends Qubee {
         }),
       ),
       unlockedWords: List<String>.from(json['unlockedWords']),
+      exampleSentence: json['exampleSentence'] ?? '',
+      meaningOfSentence: json['meaningOfSentence'] ?? '',
       requiredPoints: json['requiredPoints'],
+      isUnlocked: json['isUnlocked'] ?? false,
+      isCompleted: json['isCompleted'] ?? false,
     );
   }
 
@@ -32,11 +58,17 @@ class QubeeModel extends Qubee {
     return {
       'id': id,
       'letter': letter,
+      'smallLetter': smallLetter,
+      'latinEquivalent': latinEquivalent,
       'pronunciation': pronunciation,
       'soundPath': soundPath,
       'tracingPoints': tracingPoints,
       'unlockedWords': unlockedWords,
+      'exampleSentence': exampleSentence,
+      'meaningOfSentence': meaningOfSentence,
       'requiredPoints': requiredPoints,
+      'isUnlocked': isUnlocked,
+      'isCompleted': isCompleted,
     };
   }
 }
