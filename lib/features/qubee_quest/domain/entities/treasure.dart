2e9ccbf0a04c312096b5126e76d6aa83ac84cda0
gladsonchala <gladsonchala@gmail.com>
2025-03-18 17:45:42 +0000

    feat: add Treasure entity class

--- lib/features/qubee_quest/domain/entities/treasure.dart
+++ lib/features/qubee_quest/domain/entities/treasure.dart
@@ -1,17 +1,17 @@
 class Treasure {
   final int id;
-  final String word;
-  final String meaning;
-  final String imagePath;
   final int qubeeLetterId;
+  final String word;
+  final String exampleSentence;
+  final String meaningOfSentence;
   bool isCollected;
-
+  
   Treasure({
     required this.id,
-    required this.word,
-    required this.meaning,
-    required this.imagePath,
     required this.qubeeLetterId,
+    required this.word,
+    required this.exampleSentence,
+    required this.meaningOfSentence,
     this.isCollected = false,
   });
 }
