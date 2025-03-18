4fc97024af257e0784a09504994b28fa0785e41d
gladsonchala <gladsonchala@gmail.com>
2025-03-18 17:46:52 +0000

    rm: Removed unnecessary codes

--- lib/features/qubee_quest/domain/repositories/qubee_quest_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/qubee.dart';
-import '../entities/land.dart';
-
-abstract class QubeeQuestRepository {
-  Future<List<Qubee>> getQubeeLetters();
-  Future<List<Land>> getLands();
-}
