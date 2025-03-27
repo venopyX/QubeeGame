ec0ec1a888a54cd1be54326856d4e3684f980803
venopyX <gladsonchala@gmail.com>
2025-03-28 01:14:27 +0300

    docs(main): add documentation for application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 
+/// Application entry point
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const QubeeGameApp());
