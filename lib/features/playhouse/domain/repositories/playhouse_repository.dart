cd084cdefe2f3fc4cc0dbc0d4ebff041ff3c38d8
venopyX <gladsonchala@gmail.com>
2025-03-28 02:23:23 +0300

    refactor(repository): enhance PlayhouseRepository with additional methods and documentation

--- lib/features/playhouse/domain/repositories/playhouse_repository.dart
+++ lib/features/playhouse/domain/repositories/playhouse_repository.dart
@@ -1,7 +1,13 @@
 import '../entities/video.dart';
 
+/// Repository interface for accessing video data
 abstract class PlayhouseRepository {
+  /// Retrieves a list of all available videos
   Future<List<Video>> getVideos();
+  
+  /// Gets the ID of the last watched video
   Future<String?> getLastWatchedVideoId();
+  
+  /// Saves the ID of the last watched video
   Future<void> saveLastWatchedVideoId(String videoId);
 }
