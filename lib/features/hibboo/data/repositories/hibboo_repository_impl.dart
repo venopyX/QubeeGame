a8b65f01224dfafd554871ee8e9a06f03a62c3ed
venopyX <gladsonchala@gmail.com>
2025-03-28 01:42:11 +0300

    refactor(repository): simplify HibbooRepositoryImpl and enhance documentation

--- lib/features/hibboo/data/repositories/hibboo_repository_impl.dart
+++ lib/features/hibboo/data/repositories/hibboo_repository_impl.dart
@@ -1,20 +1,18 @@
 import '../../domain/entities/hibboo.dart';
 import '../../domain/repositories/hibboo_repository.dart';
 import '../datasources/hibboo_datasource.dart';
-import '../models/hibboo_model.dart';
 
+/// Implementation of the HibbooRepository
 class HibbooRepositoryImpl implements HibbooRepository {
+  /// The data source used to retrieve Hibboo data
   final HibbooDatasource datasource;
 
-  HibbooRepositoryImpl(this.datasource);
+  /// Creates a new HibbooRepositoryImpl instance
+  const HibbooRepositoryImpl(this.datasource);
 
   @override
   Future<List<Hibboo>> getHibbooList() async {
-    final List<HibbooModel> models = await datasource.getHibbooList();
-    return models.map((model) => Hibboo(
-      text: model.text,
-      answer: model.answer,
-      level: model.level,
-    )).toList();
+    final models = await datasource.getHibbooList();
+    return models;
   }
 }
