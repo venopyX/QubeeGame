5b2f5fb50c2fa48c7f217b357952dcb50fab3be5
venopyX <gladsonchala@gmail.com>
2025-03-28 01:40:27 +0300

    docs(datasource): add documentation for HibbooDatasource and its method

--- lib/features/hibboo/data/datasources/hibboo_datasource.dart
+++ lib/features/hibboo/data/datasources/hibboo_datasource.dart
@@ -2,7 +2,11 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import '../models/hibboo_model.dart';
 
+/// Data source for retrieving Hibboo data
 class HibbooDatasource {
+  /// Loads and parses the Hibboo data from a JSON asset file
+  ///
+  /// Returns a Future that completes with a list of [HibbooModel] objects
   Future<List<HibbooModel>> getHibbooList() async {
     final String response = await rootBundle.loadString('assets/hibboo.json');
     final List<dynamic> data = json.decode(response);
